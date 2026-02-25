54ef7cc650a4a5afd6b7ee4d747960b1c5aeec5e
Cyberkid2007 <chikwanhabrandon0@gmail.com>
2026-02-25 07:42:31 +0000

    `Added robots.txt and sitemap.xml views and templates, and updated base.html and product pages with meta tags and structured data`

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('robots.txt', views.robots_txt, name='robots_txt'),
+    path('sitemap.xml', views.sitemap_xml, name='sitemap_xml'),
     path('products/', views.products, name='products'),
     path('products/<int:pk>/', views.product_detail, name='product_detail'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from django.urls import reverse
 from .models import Product
 
 
@@ -14,3 +16,31 @@ def products(request):
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk, is_active=True)
     return render(request, 'pages/product_detail.html', {'product': product})
+
+
+def robots_txt(request):
+    sitemap_url = request.build_absolute_uri(reverse('sitemap_xml'))
+    lines = [
+        'User-agent: *',
+        'Allow: /',
+        '',
+        f'Sitemap: {sitemap_url}',
+    ]
+    return HttpResponse('\n'.join(lines), content_type='text/plain')
+
+
+def sitemap_xml(request):
+    urls = [
+        {'loc': request.build_absolute_uri(reverse('home')), 'lastmod': None},
+        {'loc': request.build_absolute_uri(reverse('products')), 'lastmod': None},
+    ]
+
+    for product in Product.objects.filter(is_active=True).only('pk', 'updated_at'):
+        urls.append(
+            {
+                'loc': request.build_absolute_uri(reverse('product_detail', args=[product.pk])),
+                'lastmod': product.updated_at.date().isoformat(),
+            }
+        )
+
+    return render(request, 'sitemap.xml', {'urls': urls}, content_type='application/xml')
